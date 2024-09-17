9ab7244725487b0955e1af1a179bcf467e060eec
Lucas <lucaslksleite@gmail.com>
2024-09-17 19:37:46 -0300

    this em php

--- screen-match/src/Modelo/Filme.php
+++ screen-match/src/Modelo/Filme.php
@@ -5,5 +5,18 @@ class Filme {
     // public me permite acessar os valores dos atributos
     public int $anoLancamento;
     public string $genero;
-    public float $nota;
+    public array $notas = [];
+
+    function avaliar (float $nota) :void{
+        $this->notas[ ] = $nota;        
+    }
+
+    function media() :float {
+        $somaNotas = array_sum($this->notas);
+        $quantidadeDeNotas = count($this->notas);
+
+        return $somaNotas / $quantidadeDeNotas;
+    }
+
 } //criando um classe. Um classe nada mais é do que um template
+  

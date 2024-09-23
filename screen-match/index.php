7e4f81e56c00833e635584433dac88cb3ff61ba0
Lucas <lucaslksleite@gmail.com>
2024-09-23 20:42:34 -0300

    getters e setters em php

--- screen-match/index.php
+++ screen-match/index.php
@@ -4,16 +4,18 @@ require __DIR__. "/src/Modelo/Filme.php";
 echo "Bem Vindo ao Screen-Match\n";
 
 $filme = new Filme();
-$filme -> nome = 'Parasita';
-$filme -> anoLancamento = 2019;
-$filme -> genero = 'Thriller';
+$filme -> setAnoLancamento(2019);
+// $filme -> nome = 'Parasita';
+// $filme -> anoLancamento = 2019;
+// $filme -> genero = 'Thriller';
 
 $filme -> avaliar(10);
 $filme -> avaliar(10);
 $filme -> avaliar(5);
 $filme -> avaliar(5);
-
 
 var_dump($filme);
 
-echo $filme ->media();
+echo $filme ->media() . "\n";
+
+echo "Lançamento:". $filme -> getAnoLancamento();
--- screen-match/src/Modelo/Filme.php
+++ screen-match/src/Modelo/Filme.php
@@ -1,22 +1,35 @@
 <?php
 
 class Filme {
-    public string $nome; 
+    private string $nome; 
     // public me permite acessar os valores dos atributos
-    public int $anoLancamento;
+    private int $anoLancamento;
+    // por exemplo, com o ano de lançamento como privado, é preciso criar um metodo (uma função) publico. Mas como atrelar um valor a propriedade privada? Com setters e getters é possivel tanto definir (set...) como resgatar(get...)
     public string $genero;
-    public array $notas = [];
+    private array $notas = [];
+    // com as propriedades sendo privadas,eu não consigo alterar fora da classe filme
 
     function avaliar (float $nota) :void{
         $this->notas[ ] = $nota;        
     }
 
-    function media() :float {
+    public function media() :float {
         $somaNotas = array_sum($this->notas);
         $quantidadeDeNotas = count($this->notas);
 
         return $somaNotas / $quantidadeDeNotas;
     }
+
+    public function setAnoLancamento(int $anoLancamento):void
+    {
+        $this -> anoLancamento = $anoLancamento;
+    }
+    // Nesse caso, set não tem retorno por estar definindo o valor da propriedade ano lançamento
+
+    public function getAnoLancamento():int{
+        return $this -> anoLancamento;
+    }
+    // Enquanto o get tem retorno por estar buscando o valor da propriedade ano lançamento 
 
 } //criando um classe. Um classe nada mais é do que um template
   

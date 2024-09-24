e770370f5f72cbcbb16cefe6ee4f310dbdb4cc07
Lucas <lucaslksleite@gmail.com>
2024-09-23 21:34:18 -0300

    readonly, getters & setter

--- screen-match/index.php
+++ screen-match/index.php
@@ -3,8 +3,12 @@ require __DIR__. "/src/Modelo/Filme.php";
 
 echo "Bem Vindo ao Screen-Match\n";
 
-$filme = new Filme();
-$filme -> setAnoLancamento(2019);
+$filme = new Filme(
+    'Parasita',
+    2021,
+    'super-heroi'
+);
+
 // $filme -> nome = 'Parasita';
 // $filme -> anoLancamento = 2019;
 // $filme -> genero = 'Thriller';
@@ -18,4 +22,10 @@ var_dump($filme);
 
 echo $filme ->media() . "\n";
 
-echo "Lançamento:". $filme -> getAnoLancamento();
+
+echo "Nome do filme: ". $filme ->nome ."\n";
+echo "Lançamento: ". $filme -> anoLancamento;
+
+// echo "Nome do filme: ". $filme -> getNome() ."\n";
+// echo "Lançamento:". $filme -> getAnoLancamento();
+// Mesmo com as propriedades privadas, consigo acessar o valor via metodos publicos (get)
--- screen-match/src/Modelo/Filme.php
+++ screen-match/src/Modelo/Filme.php
@@ -1,13 +1,26 @@
 <?php
 
 class Filme {
-    private string $nome; 
+    // private string $nome; 
     // public me permite acessar os valores dos atributos
-    private int $anoLancamento;
+    // private int $anoLancamento;
     // por exemplo, com o ano de lançamento como privado, é preciso criar um metodo (uma função) publico. Mas como atrelar um valor a propriedade privada? Com setters e getters é possivel tanto definir (set...) como resgatar(get...)
-    public string $genero;
-    private array $notas = [];
+    // public string $genero;
+    private array $notas;
     // com as propriedades sendo privadas,eu não consigo alterar fora da classe filme
+
+    public function __construct(
+        public readonly  string $nome, 
+        // readonly nada mais do que indica que propriedade em questão é somente para leitura, fazendo com ela possa ser acessada sem depender de um metodo get
+        public readonly int $anoLancamento,
+        public readonly string $genero 
+        // ao definir um modificador de acesso(private/public) a um parametro no construtor, esse parametro é "promovido" a uma propriedade, funciona SOMENTE COM O METODO CONSTRUTOR
+        )
+    {
+        $this -> notas = [];
+    }
+    // o metodo construct nada mais é do que um metodo que é chamado de forma automatica sempre que um objeto é instanciado. Não pode definir um tipo de returno já que o retorno dele é o objeto em si
+    // com o metodo construct não se faz necessario o uso do metodo set.
 
     function avaliar (float $nota) :void{
         $this->notas[ ] = $nota;        
@@ -20,16 +33,22 @@ class Filme {
         return $somaNotas / $quantidadeDeNotas;
     }
 
-    public function setAnoLancamento(int $anoLancamento):void
-    {
-        $this -> anoLancamento = $anoLancamento;
-    }
+    // public function setAnoLancamento(int $anoLancamento):void
+    // {
+    //     $this -> anoLancamento = $anoLancamento;
+    // }
     // Nesse caso, set não tem retorno por estar definindo o valor da propriedade ano lançamento
 
-    public function getAnoLancamento():int{
-        return $this -> anoLancamento;
-    }
-    // Enquanto o get tem retorno por estar buscando o valor da propriedade ano lançamento 
+    // public function getAnoLancamento():int{
+    //     return $this -> anoLancamento;
+    // }
+    // // Enquanto o get tem retorno por estar buscando o valor da propriedade ano lançamento 
+    // public function getNome() :string {
+    //     return $this -> nome;
+    // }
 
+    // public function getGenero() :string {
+    //     return $this -> genero;
+    // }
 } //criando um classe. Um classe nada mais é do que um template
   
